d943d873be2ab24d14562088e8d7ce8ca07dacb2
MafujulHaquePlabon <mafujul15-3076@diu.edu.bd>
2023-01-19 17:48:20 +0600

    javascript code added for dynamic reaction user click toggole switch btn dark and light color

---
+++ script.js
@@ -0,0 +1,23 @@
+const body = document.querySelector("body"),
+ sidebar = body.querySelector(".sidebar"),
+ toggle = body.querySelector(".toggle"),
+ searchBtn = body.querySelector(".search-box"),
+ modeSwitch = body.querySelector(".toggle-switch"),
+ modeText = body.querySelector(".mode-text");
+
+ toggle.addEventListener("click", () =>{
+     sidebar.classList.toggle("close");
+ });
+ modeSwitch.addEventListener("click", () =>{
+     body.classList.toggle("dark");
+ });
+ searchBtn.addEventListener("click" , () =>{
+    sidebar.classList.remove("close");
+});
+
+ if(body.classList.contains("dark")){
+    modeText.innerText = "Light mode";
+}
+else{
+    modeText.innerText = "Dark mode";
+};
