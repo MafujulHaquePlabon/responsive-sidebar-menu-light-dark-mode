417e29435d0cc0b5cdac3f799e27eb77da9afd91
MafujulHaquePlabon <mafujul15-3076@diu.edu.bd>
2023-01-20 11:37:37 +0600

    project done

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 const body = document.querySelector("body"),
- sidebar = body.querySelector(".sidebar"),
+ sidebar = body.querySelector("nav"),
  toggle = body.querySelector(".toggle"),
  searchBtn = body.querySelector(".search-box"),
  modeSwitch = body.querySelector(".toggle-switch"),
  modeText = body.querySelector(".mode-text");
 
- toggle.addEventListener("click", () =>{
-     sidebar.classList.toggle("close");
- });
- modeSwitch.addEventListener("click", () =>{
-     body.classList.toggle("dark");
- });
- searchBtn.addEventListener("click" , () =>{
-    sidebar.classList.remove("close");
-});
+ toggle.addEventListener("click" , () =>{
+    sidebar.classList.toggle("close");
+})
 
- if(body.classList.contains("dark")){
-    modeText.innerText = "Light mode";
-}
-else{
-    modeText.innerText = "Dark mode";
-};
+searchBtn.addEventListener("click" , () =>{
+    sidebar.classList.remove("close");
+})
+
+modeSwitch.addEventListener("click" , () =>{
+    body.classList.toggle("dark");
+    
+    if(body.classList.contains("dark")){
+        modeText.innerText = "Light mode";
+    }else{
+        modeText.innerText = "Dark mode";
+        
+    }
+});
